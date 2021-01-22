a254c075afa4ef04b5d175263a12b6d423552dd1
Wili0710 <66407599+wili0710@users.noreply.github.com>
2021-01-22 14:53:49 +0700

    write test fpr om[it success is true

--- src/input.test.js
+++ src/input.test.js
@@ -21,30 +21,38 @@ describe('render', () => {
         let wrapper
         beforeEach(() => {
             const initialState = { success: false }
-            wrapper = setup(initialState)
+            wrapper = setup( initialState )
         })
         test('renders component without error', () => {
-            const component = findByTestAttr(wrapper, "component-input")
-            expect(component.length).toBe(1)
+            const component = findByTestAttr( wrapper, "component-input" )
+            expect( component.length ).toBe(1)
         })
         test('renders input box', () => {   
-            const inputBox = findByTestAttr(wrapper, "input-box")
-            expect(inputBox.length).toBe(1)
+            const inputBox = findByTestAttr( wrapper, "input-box" )
+            expect( inputBox.length ).toBe(1)
         })
         test('renders submit button', () => {
-            const submitButton = findByTestAttr(wrapper, "component-input")
-            expect(submitButton.length).toBe(1)
+            const submitButton = findByTestAttr( wrapper, "submit-button" )
+            expect( submitButton.length ).toBe(1)
         })
     })
     describe('word has been guessed', () => {
+        let wrapper
+        beforeEach(() => {
+            const initialState = { success: true }
+            wrapper = setup( initialState )
+        })
         test('renders component without error', () => {
-
+            const component = findByTestAttr( wrapper, "component-input" )
+            expect( component.length ).toBe(1)
         })
         test('does not render input box', () => {
-            
+            const inputBox = findByTestAttr( wrapper, "input-box" )
+            expect( inputBox.length ).toBe(0)
         })
         test('does not renders submit button', () => {
-            
+            const submitButton = findByTestAttr( wrapper, "submit-button" )
+            expect( submitButton.length ).toBe(0)
         })
     })
 })
