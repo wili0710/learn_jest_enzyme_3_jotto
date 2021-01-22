b6ceef237f3715bc0bcc8b8caffed28769b427bc
Wili0710 <66407599+wili0710@users.noreply.github.com>
2021-01-22 10:49:30 +0700

    add CORRECT_GUESS action creator and test

---
+++ src/actions/index.js
@@ -0,0 +1,14 @@
+import { func } from "prop-types"
+
+export const actionTypes = {
+    CORRECT_GUESS: 'CORRECT_GUESS'
+}
+
+
+/**
+ * @function correctGuess
+ * @return {object} - Action object with type "CORRECT_GUESS"
+ */
+export function correctGuess() {
+    return { type:actionTypes.CORRECT_GUESS}
+}
